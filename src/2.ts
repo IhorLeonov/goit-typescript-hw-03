2ed92c761514a15bd170eef957ff1a244af5638f
Ihor Leonov <ihorleonow@gmail.com>
2023-10-08 20:54:34 +0300

    ADD | 1, 2 tasks

--- src/2.ts
+++ src/2.ts
@@ -11,16 +11,17 @@
   Клас Manager повинен бути підклас класу Employee
 
   Необхідно реалізувати в класі Manager конструктор, який викликатиме конструктор суперкласу та збільшуватиме salary на 10000.
-
+  
 */
 
 class Employee {
   // Заповніть модифікатори доступу
-  name: string;
-  department: string;
-  salary: number;
 
-  constructor(name: string, department: string, salary: number) {
+  constructor(
+    public name: string,
+    private department: string,
+    protected salary: number
+  ) {
     this.name = name;
     this.department = department;
     this.salary = salary;
@@ -33,7 +34,16 @@ class Employee {
 
 class Manager extends Employee {
   // Реалізуйте конструктор та збільшіть salary на 10000
+
+  constructor(name: string, department: string, salary: number) {
+    super(name, department, salary);
+
+    this.salary = this.salary + 10000;
+
+    console.log(this.getEmployeeDetails());
+  }
 }
 
+const stoneHouse = new Manager("Ihor", "IT", 5000); // Name: Ihor, Department: IT, Salary: 15000
 
 export {};
